a118de51e458d07557c8237a139c51035f78498e
胡伟煌 <huweihuang@foxmail.com>
2023-08-08 10:23:18 +0800

    add kubeadm cert (#5)

---
+++ kubeadm/update-kubeadm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+Version=$1
+Version=${Version:-1.24.2}
+
+# 备份kubeadm
+timestamp=$(date "+%Y%m%d%l%M%S")
+mv /usr/bin/kubeadm /usr/bin/kubeadm.${timestamp} 2>/dev/null || true
+
+# 下载安装10年证书的kubeadm版本
+wget https://github.com/huweihuang/kubeadm-scripts/releases/download/kubeadm/kubeadm-v${Version} -O /usr/bin/kubeadm
+chmod +x /usr/bin/kubeadm
+
+# 查看版本
+kubeadm version
