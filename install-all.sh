792411c801acef0a615c5c3aa1903353455cf96b
huweihuang <huweihuang@foxmail.com>
2023-08-27 21:47:24 +0800

    add setup-k8s scripts

--- install-all.sh
+++ install-all.sh
@@ -8,8 +8,11 @@ NodeType=${1:-node}
 K8sVersion=${2:-1.24.2}
 ContainerdVersion=${3:-1.6.8}
 RuncVersion=${4:-1.1.4}
+CniVersion=${5:-1.1.1}
+NerdctlVersion=${6:-1.1.0}
+CrictlVersion=${7:-1.26.0}
 
 # 下载节点套件
 bash kubeadm/kubeadm-init.sh ${NodeType}
-bash containerd/install-containerd.sh ${ContainerdVersion} ${RuncVersion}
+bash containerd/install-containerd.sh ${ContainerdVersion} ${RuncVersion} ${CniVersion} ${NerdctlVersion} ${CrictlVersion}
 bash kubeadm/install-kubeadm.sh ${K8sVersion}
---
+++ setup-k8s.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+set -e
+
+NodeType=
+MasterDomain=
+MasterIP=
+NodeName=
+Token=
+Hash=
+CertificateKey=
+
+
+# 版本
+K8sVersion=$(grep K8sVersion version|awk -F "=" '{print $2}')
+ContainerdVersion=$(grep ContainerdVersion version|awk -F "=" '{print $2}')
+RuncVersion=$(grep RuncVersion version|awk -F "=" '{print $2}')
+CniVersion=$(grep CniVersion version|awk -F "=" '{print $2}')
+NerdctlVersion=$(grep NerdctlVersion version|awk -F "=" '{print $2}')
+CrictlVersion=$(grep CrictlVersion version|awk -F "=" '{print $2}')
+
+
+function usage(){
+	shellname="`echo ${0##*/}`"
+
+	echo -e "usage:
+	-h                         显示帮助
+	-t [NodeType]              节点类型：(master, node)
+	-d [MasterDomain]          apiserver域名
+	-i [MasterIP]              Master IP/VIP
+	-n [NodeName]              节点名称
+	-k [Token]                 添加master/node的Token
+	-a [Hash]                  添加master/node的Hash
+	-c [CertificateKey]        添加master的CertificateKey
+"
+
+	echo "example:
+
+bash setup-k8s.sh \\
+-t master \\
+-d api.k8s.io \\
+-i 1.1.1.1 \\
+-n 1.1.1.1
+"
+	exit
+}
+
+while getopts "t:d:i:n:i:k:a:c:h" opt
+do
+    case $opt in
+        t) NodeType=$OPTARG
+        echo "NodeType VALUE: $OPTARG"
+        ;;
+        d) MasterDomain=$OPTARG
+        echo "MasterDomain VALUE: $OPTARG"
+        ;;
+        i) MasterIP=$OPTARG
+        echo "MasterIP VALUE: $OPTARG"
+        ;;
+        n) NodeName=$OPTARG
+        echo "NodeName VALUE: $OPTARG"
+        ;;
+        k) Token=$OPTARG
+        echo "Token VALUE: $OPTARG"
+        ;;                    
+        a) Hash=$OPTARG
+        echo "Hash VALUE: $OPTARG"
+        ;;
+        c) CertificateKey=$OPTARG
+        echo "CertificateKey VALUE: $OPTARG"
+        ;;
+		h) usage;;
+		?) usage;;
+    esac
+done
+
+
+function init_node(){
+    echo "[=======install kubelet and containerd=======]"
+    # 设置域名本地解析
+    sed -i '/${MasterDomain}/d' /etc/hosts
+    echo "${MasterIP} ${MasterDomain}" >> /etc/hosts
+
+    # 安装conntrack
+    apt -y install conntrack
+
+    # 安装containerd kubelet组件
+    bash install-all.sh ${NodeType} ${K8sVersion} ${ContainerdVersion} ${RuncVersion} ${CniVersion} ${NerdctlVersion} ${CrictlVersion}
+}
+
+function init_master(){
+    echo "[================init master================]"
+    # 下载kubeadm-config.yaml并修改MasterDomain和Version
+    wget https://raw.githubusercontent.com/huweihuang/kubeadm-scripts/main/kubeadm/kubeadm-config.yaml
+    sed -i "s|_MasterDomain_|${MasterDomain}|g;
+    s|_K8sVersion_|${K8sVersion}|g" kubeadm-config.yaml
+
+    # kubeadm init 创建第一个master节点
+    kubeadm init --config kubeadm-config.yaml --upload-certs  --node-name ${NodeName}
+}
+
+# 添加节点
+function join_node(){
+    echo "[================join node================]"
+    kubeadm join ${MasterDomain}:6443 --token ${Token} \
+    --discovery-token-ca-cert-hash sha256:${Hash} \
+    --node-name ${NodeName}
+}
+
+# 添加master
+function join_master(){
+    echo "[================join master================]"
+    kubeadm join ${MasterDomain}:6443 --token ${Token} \
+    --discovery-token-ca-cert-hash sha256:${Hash} \
+    --control-plane --certificate-key ${CertificateKey} \
+    --node-name ${NodeName}
+}
+
+main(){
+   init_node
+
+   case ${NodeType} in
+   "master")
+        # 如果以下参数都为空，则init master
+        if [ -z ${Token} ] && [ -z ${Hash} ] && [ -z ${CertificateKey} ]; then
+            init_master
+        # 如果以下参数都不为空，则join master
+        elif [ ${Token} ] && [ ${Hash} ] && [ ${CertificateKey} ]; then
+            join_master
+        else
+            echo "invalid Token,Hash,CertificateKey"
+        fi
+        ;;
+   "node")
+        join_node
+        ;;
+   *)
+        echo "invalid NodeType"
+        ;;
+   esac
+}
+
+main
