543566e5aa52f69b87ff39574b498d034b89b4ce
huweihuang <huweihuang@foxmail.com>
2023-09-03 12:04:38 +0800

    fix install-calico.sh

--- cni/install-calico.sh
+++ cni/install-calico.sh
@@ -13,7 +13,7 @@ sed -i 's/192.168.0.0\/16/10.244.0.0\/16/g' calico.yaml
 
 # 安装calico
 kubectl create ns calico-system || true
-kubectl create -f calico.yaml
+kubectl apply -f calico.yaml
 
 # 安装calicoctl
 wget https://github.com/projectcalico/calico/releases/latest/download/calicoctl-linux-amd64 -O /usr/local/bin/calicoctl && chmod +x /usr/local/bin/calicoctl
