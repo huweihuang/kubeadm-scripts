c02760daec70334f1caed61207459c64eec5c137
胡伟煌 <huweihuang@foxmail.com>
2025-09-18 21:10:45 +0800

    Update install-docker-by-bin.sh

--- docker/install-docker-by-bin.sh
+++ docker/install-docker-by-bin.sh
@@ -4,12 +4,25 @@ set -e
 # reference: https://docs.docker.com/engine/install/binaries/
 # https://download.docker.com/linux/static/stable/
 
-Version=${1:-20.10.18}
+Version=${1:-28.4.0}
 
+# download docker
 download_url="https://download.docker.com/linux/static/stable/x86_64/docker-${Version}.tgz"
+filename="docker-${Version}.tgz"
 
-wget ${download_url}
-tar xzvf docker-${Version}.tgz
+if [ -f "$filename" ]; then
+    echo "✅ 文件 $filename 已存在，跳过下载。"
+else
+    echo "📥 正在从 $download_url 下载..."
+    wget ${download_url}
+fi
+
+rm -rf docker
+tar xzvf ${filename}
+
+# remove old docker
+rm -rf /usr/bin/docker* /usr/bin/containerd* /usr/bin/ctr /usr/bin/runc
+# copy new docker
 cp -fr docker/* /usr/bin
 
 # setup docker.service
@@ -63,7 +76,55 @@ OOMScoreAdjust=-500
 WantedBy=multi-user.target
 EOF
 
+cat > /lib/systemd/system/docker.socket << EOF
+[Unit]
+Description=Docker Socket for the API
+
+[Socket]
+# If /var/run is not implemented as a symlink to /run, you may need to
+# specify ListenStream=/var/run/docker.sock instead.
+ListenStream=/run/docker.sock
+SocketMode=0660
+SocketUser=root
+SocketGroup=docker
+
+[Install]
+WantedBy=sockets.target
+EOF
+
+# setup containerd.service
+cat > /lib/systemd/system/containerd.service << EOF
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network.target local-fs.target
+
+[Service]
+ExecStartPre=-/sbin/modprobe overlay
+ExecStart=/usr/bin/containerd
+
+Type=notify
+Delegate=yes
+KillMode=process
+Restart=always
+RestartSec=5
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitMEMLOCK=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+LimitNOFILE=infinity
+# Comment TasksMax if your systemd version does not supports it.
+# Only systemd 226 and above support this version.
+TasksMax=infinity
+OOMScoreAdjust=-999
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 # start docker
+systemctl daemon-reload
 systemctl enable docker
 systemctl restart docker
 systemctl status docker
